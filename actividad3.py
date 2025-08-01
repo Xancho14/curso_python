54557f03d2c6a838f3eed91191f1ccc3a1e4b8fc
Xancho14 <ln2006ce@gmail.com>
2025-08-01 09:14:18 -0300

    codigo clases

---
+++ actividad3.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+@dataclass
+class Numero:
+    numero: int
+        
+    def evaluarNumero(self):
+        if self.numero & 1:
+            print("El numero es Impar")
+            if self.numero == 7:
+                print("/nEl numero es un Comodin")
+        else:
+            print("El numero es Par")
+            if self. numero == 10:
+                print("/nEl numero es 10")
+                
+    def sumar(self,numeroasumar):
+        return self. numero + numeroasumar
+    
+if __name__ == "__main__":
+    numeroaevaluar = Numero(int(input("Ingrese un numero:/n ")))
+    numeroaevaluar.evaluarNumero()
+    sumarealizada = numeroaevaluar.sumar(2)
+    print("/nLa suma realizada es: ",sumarealizada)
